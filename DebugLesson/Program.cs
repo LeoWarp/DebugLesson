cf7ad8c4f4a0cd615a6498fbf755a3daeab67b5b
LeoWarp <leowarp@bk.ru>
2021-09-28 20:25:23 +0300

    first Commit

---
+++ DebugLesson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DebugLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+            var name = Console.ReadLine();
+            
+            // Нужно вывести все данные пользователя
+            var user = GetUser(name);
+            Console.WriteLine($"Имя пользователя - {user.Name} | Возраст пользователя - {user.Age}");
+
+            Console.WriteLine(new string('-', 25));
+            UserService.Print();
+        }
+
+        private static User GetUser(string name)
+        {
+            var user = UserService.GetUser(name);
+            if (user != null)
+                return user;
+
+            return null;
+        }
+    }
+}
---
+++ DebugLesson/UserService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DebugLesson
+{
+    public class User
+    {
+        public string Name { get; set; }
+        
+        public int Age { get; set; }
+    }
+    
+    public static class UserService
+    {
+        public static List<User> Users = new List<User>()
+        {
+            new User() { Name = "Alex", Age = 18 },
+            new User() { Name = "Andrey" },
+            new User() { Name = "John" }
+        };
+
+        public static User GetUser(string name)
+        {
+            return Users.FirstOrDefault(x => x.Name == name);
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("Дай угадаю, опять пишешь говнокод? Не говорили только, что нет!");
+        }
+    }
+}
