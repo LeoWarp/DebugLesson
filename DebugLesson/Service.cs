1d4d34eb983c8a1b83a9b9ceae0e787d238977c9
LeoWarp <leowarp@bk.ru>
2021-09-28 21:12:21 +0300

    added values

--- DebugLesson/Service.cs
+++ DebugLesson/Service.cs
@@ -2,6 +2,10 @@
 {
     public class Service
     {
+        public string Name { get; set; }
         
+        public string Namee { get; set; }
+        
+        public string Nameee { get; set; }
     }
 }
